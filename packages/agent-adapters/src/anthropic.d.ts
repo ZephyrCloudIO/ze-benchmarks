35a59085823bdcdff89bd438d195ac773b8a41db
Nitin <nitink2003@gmail.com>
2025-10-10 08:39:21 -0700

    chore:clean TS files

--- packages/agent-adapters/src/anthropic.d.ts
+++
@@ -1,8 +0,0 @@
-import type { AgentAdapter, AgentRequest, AgentResponse } from "./index.js";
-export declare class AnthropicAdapter implements AgentAdapter {
-    name: string;
-    private client;
-    constructor(apiKey?: string);
-    send(request: AgentRequest): Promise<AgentResponse>;
-    private estimateCost;
-}
