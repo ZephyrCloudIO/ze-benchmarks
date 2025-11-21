e6cb28c378ec5ff9dbecb08991cc800aa4f6e69f
DongYun Kang <kdy.1997.dev@gmail.com>
2025-11-21 11:07:15 -0500

    feat: Initialize definer

---
+++ apps/definer/rsbuild.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from '@rsbuild/core';
+import { withZephyr } from "zephyr-rsbuild-plugin";
+import { pluginReact } from '@rsbuild/plugin-react';
+
+// Docs: https://rsbuild.rs/config/
+export default defineConfig({
+  plugins: [pluginReact(), withZephyr()]
+});
